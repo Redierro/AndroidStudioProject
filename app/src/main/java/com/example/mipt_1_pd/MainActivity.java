70c1cb3f601bf8c72b64b1431aa74ebaa83d11c6
Redierro <paulius360wow@gmail.com>
2025-09-22 12:51:13 +0300

    Antrasis issaugojimas, Textview ir button changes

--- app/src/main/java/com/example/mipt_1_pd/MainActivity.java
+++ app/src/main/java/com/example/mipt_1_pd/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mipt_1_pd;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Kodas kuris pakeicia teksta
+        TextView tv = findViewById(R.id.textViewHello);
+        Button btnChangeText = findViewById(R.id.buttonChangeText);
+
+        btnChangeText.setOnClickListener(v -> {
+            tv.setText("Testavimas! MIPT Praktika testas!");
         });
     }
 }
